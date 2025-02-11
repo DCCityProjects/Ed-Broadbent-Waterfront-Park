7ec0547e5cae438a4dc2cd1586b80a83f234933e
Charlize San Gabriel <charlizesangabriel@gmail.com>
2025-02-11 17:36:29 -0500

    Adjusted the page.js classes, and fixed the landing and global.css. Trying to also install the font with next/font

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,26 @@
 import Link from "next/link";
 import "../css/landing.css";
 
+
 export default function LandingPage() {
   return (
     <main>
-      <header>
+      <header className="">
         <button>Burger Menu</button>
         <button>Logo</button>
       </header>
-      <section className="welcome-section">
-        <h1>Welcome</h1>
-        <p>Explore, navigate, and enjoy all the park has to offer with ease!</p>
-        <button><Link href="/map">Go to map</Link></button>
+      <section className="welcome-section u-flex-column-justify-align-center">
+        <h1 className="welcome-section__title">Welcome</h1>
+        <p className="welcome-section__description">Explore, navigate, and enjoy all the park has to offer with ease!</p>
+        <button><Link href="/map" className="welcome-section__button">GO TO MAP</Link></button>
       </section>
-      <section className="nav-section">
-        <h2>Explore Ed Broadbent Park</h2>
-        <nav>
-          <ul>
-            <li><Link href="/general" >General Information</Link></li>
-            <li><Link href="/aboutEdBroadbent" >About Ed Broadbent</Link></li>
-            <li><Link href="/events" >Events and Activities</Link></li>
+      <section className="home-nav-section">
+        <h2 className="home-nav-section__title">Explore Ed Broadbent Park</h2>
+        <nav className="home-nav">
+          <ul className="home-nav__list">
+            <li><Link href="/general" className="home-nav__link">General Information</Link></li>
+            <li><Link href="/aboutEdBroadbent" className="home-nav__link">About Ed Broadbent</Link></li>
+            <li><Link href="/events" className="home-nav__link">Events and Activities</Link></li>
           </ul>
         </nav>
       </section>
