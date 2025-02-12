c883d2163db427eff58152dc6bcccb58a2725c84
Charlize San Gabriel <charlizesangabriel@gmail.com>
2025-02-11 22:02:32 -0500

    Began adding map. Haven't integrated the popups

--- src/app/map/page.js
+++ src/app/map/page.js
@@ -1,35 +1,30 @@
 import Link from "next/link";
-import "../../css/map.css";
+import "../css/map.css";
 import icone from "/public/images/icone.jpg"
 import Image from "next/image";
-
+import LeafletMap from "./LeafletMap";
 
 export default function Map() {
     return (
         <main>
-            
-            <header>
-                <button>Burger Menu</button>
-                <button>Logo</button>
-            </header>
+            <LeafletMap />
             <h1>Map</h1>
             <section>
                 <h2>Select Destination</h2>
                 <div className="select-destination-input-container">
                     <div className="from-input-field"> 
-                        <Image src={icone} className="search-icon"></Image>
+                        <Image src={icone} className="search-icon" alt="football"></Image>
                         <input defaultValue="From" type="text" />
-                        <Image src={icone} className="search-icon"></Image>
+                        <Image src={icone} className="search-icon" alt="football"></Image>
                     </div>
                     <div className="from-input-field">
-                        <Image src={icone} className="search-icon"></Image>
+                        <Image src={icone} className="search-icon" alt="football"></Image>
                         <input defaultValue="To" type="text" name="fromStartingPoint" />
-                        <Image src={icone} className="search-icon"></Image>
+                        <Image src={icone} className="search-icon" alt="football"></Image>
                     </div>
                 </div>
                 <button>GO</button>
             </section>
-
         </main>
     );
 }
