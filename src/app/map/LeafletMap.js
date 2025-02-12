7575204225ab6abdcb7239e9e754b5f806566ecc
Charlize San Gabriel <charlizesangabriel@gmail.com>
2025-02-12 12:08:52 -0500

    Added a dynamic thingy from stackoverflow

--- src/app/map/LeafletMap.js
+++ src/app/map/LeafletMap.js
@@ -3,14 +3,16 @@
 import { ImageOverlay, MapContainer } from "react-leaflet";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function LeafletMap() {
     const [zoom, setZoom] = useState(0);
     const [center, setCenter] = useState([500, 200]);
 
+
     const bounds = [[0, 0], [2592, 4326]];
     const panBounds = [[-200,-200], [2992,4726]];
+
 
     return (
         <div id="map">
--- src/app/map/page.js
+++ src/app/map/page.js
@@ -1,26 +1,30 @@
+"use client"
+
 import Link from "next/link";
 import "../css/map.css";
 import icone from "/public/images/icone.jpg"
 import Image from "next/image";
-import LeafletMap from "./LeafletMap";
+import dynamic from "next/dynamic";
 
 export default function Map() {
+    const LeafletMap = dynamic(() => import('./LeafletMap'), {ssr: false});
+    
     return (
         <main>
             <LeafletMap />
             <h1>Map</h1>
-            <section>
+            <section className="popup">
                 <h2>Select Destination</h2>
-                <div className="select-destination-input-container">
-                    <div className="from-input-field"> 
-                        <Image src={icone} className="search-icon" alt="football"></Image>
-                        <input defaultValue="From" type="text" />
-                        <Image src={icone} className="search-icon" alt="football"></Image>
+                <div className="navigation-fields">
+                    <div className="navigation-fields__row"> 
+                        <Image src={icone} className="navigation-fields__icon" alt="football"></Image>
+                        <input defaultValue="From" type="text" className="navigation-fields__input" />
+                        <Image src={icone} className="navigation-fields__mic" alt="football"></Image>
                     </div>
-                    <div className="from-input-field">
-                        <Image src={icone} className="search-icon" alt="football"></Image>
-                        <input defaultValue="To" type="text" name="fromStartingPoint" />
-                        <Image src={icone} className="search-icon" alt="football"></Image>
+                    <div className="navigation-fields__row">
+                        <Image src={icone} className="navigation-fields__icon" alt="football"></Image>
+                        <input defaultValue="To" type="text" className="navigation-fields__input" name="fromStartingPoint" />
+                        <Image src={icone} className="navigation-fields__icon" alt="football"></Image>
                     </div>
                 </div>
                 <button>GO</button>
