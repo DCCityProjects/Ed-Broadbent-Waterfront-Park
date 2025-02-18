c643279fcd7ff335ed732df8e8ed4fabd2c9c5dc
ReiGoxheri <kingshow776@gmail.com>
2025-02-17 23:24:20 -0500

    Basic header elements

--- src/app/general/page.js
+++ src/app/general/page.js
@@ -1,9 +1,9 @@
 const General = () => {
     return ( 
-        <div>
+        <>
             <h1>General</h1>
-        </div>
-     );
+        </>
+    );
 }
- 
+
 export default General;
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { PT_Sans } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
+import Image from "next/image";
 
 const ptSans = PT_Sans({
 weight: ["400", "700"],
@@ -20,10 +21,23 @@ return (
 			<header className="nav">
 				<ul className="nav__list">
 					<li className="nav__item">
-						<button className="nav__link nav__burger">Burger Menu</button>
+						<Image 
+							src="/images/hamburger.svg" 
+							alt="Hamburger Menu" 
+							width={40} 
+							height={40} 
+							className="nav__hamburger"
+						/>
 					</li>
 					<li className="nav__item">
-						<Link href="/" className="nav__link">Logo</Link>
+						<Link href="/" className="nav__link nav__logo">
+							<Image 
+								src="/images/logo.svg" 
+								alt="Navigation Logo" 
+								width={40} 
+								height={40}
+							/>
+						</Link>
 					</li>
 				</ul>
 			</header>
