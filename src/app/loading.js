8cef793285ffebbcf447e78bb2b253366499d4b1
Charlize <145717754+NyxVamour@users.noreply.github.com.>
2025-06-24 11:44:31 -0400

    refactor(ui): adjust landing & loading
    Created components for landing and loading pages.
    Changed the timer functionality.
    Refactored the loading to use next js loading.
    Changed some class names to make it BEM.

--- src/app/loading.js
+++ src/app/loading.js
@@ -3,28 +3,33 @@
 import Image from "next/image";
 import gsap from "gsap";
 import { useEffect } from "react";
+import LoadingLogo from "./components/svgs/loadingLogo.js";
 
 
 export default function Loading() {
 
     useEffect(()=>{
         const tl = gsap.timeline({});
+        console.log("tl test")
+        tl.restart();
         tl
-            .to(".loading-logo", {
+            .to(".loading__logo", {
                 duration: 0.5,
+                scale: 2,
                 opacity: 1,
                 ease: "power1.in",
             })
     })
 
     return (
-        <section>
+        <section className="loading">
+            {/* <LoadingLogo className="loading__logo" /> */}
             <Image
-                src="/Ed-Broadbent-Waterfront-Park/images/svgs/logo_loading.svg"
+                src="/Ed-Broadbent-Waterfront-Park/images/svgs/logo-text-square-transparent.svg"
                 alt="Loading..."
                 width={100}
                 height={100}
-                className="loading-logo"
+                className="loading__logo"
                 opacity={0}
             />
         </section>
