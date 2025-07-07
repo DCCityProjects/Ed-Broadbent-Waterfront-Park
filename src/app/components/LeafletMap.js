8e2e5dd77eaae6fc0f3c6fa3b136d262b6548629
Charlize <145717754+NyxVamour@users.noreply.github.com.>
2025-07-07 17:42:03 -0400

    refactor: fix handleClick & iconState & props
    Added stateList for the props.
    Made handleClick use useCallback.
    Changed if statement to switch case for newIconState.
    Removed markers array.
    Updated iconState.

--- src/app/components/LeafletMap.js
+++ src/app/components/LeafletMap.js
@@ -3,105 +3,94 @@
 import { ImageOverlay, MapContainer, Marker, Popup, Tooltip, useMap, useMapEvent } from "react-leaflet";
 import L, { marker } from "leaflet";
 import "leaflet/dist/leaflet.css";
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import {
-    iconAmphitheatreGrey,
-    iconAmphitheatreSelect,
-    iconBenchGrey,
-    iconBenchSelect,
-    iconBridgeGrey,
-    iconBridgeSelect,
-    iconGardenHRGrey,
-    iconGardenHRSelect,
-    iconOrangeGardenGrey,
-    iconOrangeGardenSelect,
-    iconParkingGrey,
-    iconParkingSelect,
-    iconMainGrey,
-    iconMainSelect
+    iconAmpGrey, iconAmpCol,
+    iconBenchesGrey, iconBenchesCol,
+    iconBikeGrey, iconBikeCol,
+    iconBridgeGrey, iconBridgeCol,
+    iconEBGrey, iconEBCol,
+    iconHRGrey, iconHRCol,
+    iconIGrey, iconICol,
+    iconMapGrey, iconMapCol,
+    iconOGGrey, iconOGCol,
+    iconParkingGrey, iconParkingCol
 } from "@/app/components/mapPins"
 import MapPin from "./MapPin";
 import ZoomTool from "./ZoomTool";
 import gsap from "gsap";
-import RecenterAutomatically from "./RecenterAutomatically";
+import RecenterAutomatically from "./map/RecenterAutomatically";
 import { useSearchParams } from "next/navigation";
 
 
-export default function LeafletMap({setContent, popupRef, resetIcons, isIconClicked, setIsIconClicked, iconState, setIconState}) {
+export default function LeafletMap({stateList}) {
     const [zoom, setZoom] = useState(0);
-    const [center, setCenter] = useState([4300, 550]);
+    const [center, setCenter] = useState([4150, 210]);
     const [iconSize, setIconSize] = useState(50);
-    // const [iconState, setIconState] = useState([]);
     const [iconList, setIconList] = useState([]);
     const [offset, setOffset] = useState(0);
 
-    // const [L, setL] = useState(null);
+    const {
+        setContent, iconState,
+        popupRef, resetIcons, 
+        isIconClicked, setIsIconClicked,
+        setIconState
+    } = stateList;
+
 
     const searchParams = useSearchParams();
 
-    useEffect(()=>{
-        setIconState([
-            {icon: iconMainGrey, iconGrey:iconMainGrey, iconSelect: iconMainSelect, name: "Main Entrance", permanent: false},
-            {icon: iconMainGrey, iconGrey: iconMainGrey, iconSelect: iconMainSelect, name: "About Ed Broadbent", permanent: false},
-            {icon: iconAmphitheatreGrey, iconGrey: iconAmphitheatreGrey, iconSelect: iconAmphitheatreSelect, name: "Amphitheatre and Stage", permanent: false},
-            {icon: iconGardenHRGrey, iconGrey:iconGardenHRGrey, iconSelect: iconGardenHRSelect, name: "Garden of Human Rights", permanent: false},
-            {icon: iconOrangeGardenGrey, iconGrey: iconOrangeGardenGrey, iconSelect: iconOrangeGardenSelect, name: "Orange Garden", permanent: false},
-            {icon: iconMainGrey, iconGrey: iconMainGrey, iconSelect: iconMainSelect, name: "Parking Entrance", permanent: false}
-        ])
-    }, [setIconState])
-
-    useEffect(()=>{
-        const contentParam = searchParams.get("content");
-        if(contentParam){
-            setContent(contentParam)
-            const markerIndex = markers.findIndex(marker=>marker.url === contentParam);
-            if(markerIndex !== -1){
-                const marker = markers[markerIndex];
-                setCenter(marker.position);
-                handleClick(null, marker, markerIndex);
-            }
-            console.log(contentParam)
-        }
-    },[])
-
-
-    const bounds = [[0, 0], [4779, 2868]];
-    const panBounds = [[-3000, -3000], [7779, 5868]];
-
-    // useEffect(()=>{
-    //     console.log(offset)
-    // },[offset])
-
-    
-
-
-    useEffect(()=>{
-        console.log(iconState)
-    }, [iconState])
-
-    // const resetIcons = ()=>{
-    //     iconState.map((icon, index)=>{
-    //         icon.icon = icon.iconGrey;
-    //     })
-    // }
-
-
-    const handleClick = (e, marker, index) => {
+    const handleClick = useCallback((e, marker, index) => {
         console.log(`clicked ${marker.name}`);
-        if(iconState[index]){
+        if(!iconState[index]){
+            console.log("No icon state");
+            return
+        };
             console.log(iconState[index].icon);
             setIsIconClicked(false);
             resetIcons(iconState);
 
             const newIconState = [...iconState];
-            if (iconState[index].icon === iconMainGrey) {
-                newIconState[index].icon = iconMainSelect;
-            } else if (iconState[index].icon === iconAmphitheatreGrey) { 
-                newIconState[index].icon = iconAmphitheatreSelect;
-            } else if (iconState[index].icon === iconGardenHRGrey) {
-                newIconState[index].icon = iconGardenHRSelect;
-            } else if (iconState[index].icon === iconOrangeGardenGrey) {
-                newIconState[index].icon = iconOrangeGardenSelect;
+
+            switch (iconState[index].icon) {
+                case iconAmpGrey:
+                    newIconState[index].icon = iconAmpCol;
+                    break;
+
+                case iconBenchesGrey:
+                    newIconState[index].icon = iconBenchesCol;
+                    break;
+            
+                case iconBikeGrey:
+                    newIconState[index].icon = iconBikeCol;
+                    break;
+
+                case iconBridgeGrey:
+                    newIconState[index].icon = iconBridgeCol;
+                    break;
+
+                case iconEBGrey:
+                    newIconState[index].icon = iconEBCol;
+                    break;
+
+                case iconHRGrey:
+                    newIconState[index].icon = iconHRCol;
+                    break;
+
+                case iconMapGrey:
+                    newIconState[index].icon = iconMapCol;
+                    break;
+
+                case iconOGGrey:
+                    newIconState[index].icon = iconOGCol;
+                    break;
+
+                case iconParkingGrey:
+                    newIconState[index].icon = iconParkingCol;
+                    break;
+
+                default:
+                    break;
             }
 
             setIconState(newIconState);
@@ -115,62 +104,197 @@ export default function LeafletMap({setContent, popupRef, resetIcons, isIconClic
             } else {
                 console.log("no popupRef")
             }
-        } else {
-            console.log("No icon state")
-        }
         
-    }
+    }, [iconState, resetIcons, setIsIconClicked, setIconState, setContent, popupRef])
 
-    const markers = [
-        {
-            position: [4300,550],
-            zIndexOffset: 1000,
-            iconGrey: iconMainGrey,
-            iconSelect: iconMainSelect,
-            name: "Main Entrance",
-            url: "main-entrance"
-        },
-        {
-            position: [4035,570],
-            zIndexOffset: 1000,
-            iconGrey: iconMainGrey,
-            iconSelect: iconMainSelect,
-            name: "About Ed Broadbent",
-            url: "about-ed-broadbent"
-        },
-        {
-            position: [3520,947],
-            zIndexOffset: 1000,
-            iconGrey: iconAmphitheatreGrey,
-            iconSelect: iconAmphitheatreSelect,
-            name: "Amphitheatre and Stage",
-            url: "amphitheatre-and-stage"
-        },
-        {
-            position: [3200,980],
-            zIndexOffset: 1000,
-            iconGrey: iconGardenHRGrey,
-            iconSelect: iconGardenHRSelect,
-            name: "Garden of Human Rights",
-            url: "garden-of-human-rights"
-        },
-        {
-            position: [920,1680],
-            zIndexOffset: 1000,
-            iconGrey: iconOrangeGardenGrey,
-            iconSelect: iconOrangeGardenSelect,
-            name: "Orange Garden",
-            url: "orange-garden"
-        },
-        {
-            position: [465,1570],
-            zIndexOffset: 1000,
-            iconGrey: iconMainGrey,
-            iconSelect: iconMainSelect,
-            name: "Parking Entrance",
-            url: "parking-entrance"
+    useEffect(()=>{
+        setIconState([
+            {
+                position: [3520, 750],
+                zIndexOffset: 1000,
+                icon: iconAmpGrey,
+                iconCol: iconAmpCol,
+                name: "Amphitheatre and Stage",
+                url: "amphitheatre-and-stage",
+                permanent: false
+            },
+            {
+                position: [550, 1800],
+                zIndexOffset: 1000,
+                icon: iconBenchesGrey,
+                iconCol: iconBenchesCol,
+                name: "Benches",
+                url:"benches",
+                permanent: false
+            },
+            {
+                position: [3500, 310],
+                zIndexOffset: 1000,
+                icon: iconBikeGrey,
+                iconCol: iconBikeCol,
+                name: "Bike Parking",
+                url:"bike-parking",
+                permanent: false
+            },
+            {
+                position: [3740, 1400],
+                zIndexOffset: 1000,
+                icon: iconBikeGrey,
+                iconCol: iconBikeCol,
+                name: "Bike Parking",
+                url:"bike-parking",
+                permanent: false
+            },
+            {
+                position: [1500, 1720],
+                zIndexOffset: 1000,
+                icon: iconBridgeGrey,
+                iconCol: iconBridgeCol,
+                name: "Larry Ladd Bridge",
+                url: "bridge",
+                permanent: false
+            },
+            {
+                position: [4035, 300],
+                zIndexOffset: 1000,
+                icon: iconEBGrey,
+                iconCol: iconEBCol,
+                name: "About Ed Broadbent",
+                url: "about-ed-broadbent",
+                permanent: false
+            },
+            {
+                position: [3300, 750],
+                zIndexOffset: 1000,
+                icon: iconHRGrey,
+                iconCol: iconHRCol,
+                name: "Garden of Human Rights",
+                url: "garden-of-human-rights",
+                permanent: false
+            },
+            // {
+            //     position: [465,1570],
+            //     zIndexOffset: 1000,
+            //     icon: iconIGrey,
+            //     iconCol: iconICol,
+            //     name: "Information",
+            //     permanent: false
+            // },
+            {
+                position: [4400, 350],
+                zIndexOffset: 1000,
+                icon: iconMapGrey,
+                iconCol: iconMapCol,
+                name: "Main Map North",
+                url: "main-map",
+                permanent: false
+            },
+            {
+                position: [440, 1550],
+                zIndexOffset: 1000,
+                icon: iconMapGrey,
+                iconCol: iconMapCol,
+                name: "Main Map",
+                url: "main-map",
+                permanent: false
+            },
+            {
+                position: [920, 1600],
+                zIndexOffset: 1000,
+                icon: iconOGGrey,
+                iconCol: iconOGCol,
+                name: "Orange Garden",
+                url: "orange-garden",
+                permanent: false
+            },
+            {
+                position: [168, 975],
+                zIndexOffset: 1000,
+                icon: iconParkingGrey,
+                iconCol: iconParkingCol,
+                name: "Parking",
+                url: "parking",
+                permanent: false
+            },
+        ])
+    }, [setIconState])
+
+    useEffect(()=>{
+        const contentParam = searchParams.get("content");
+        if(contentParam){
+            setContent(contentParam)
+            const markerIndex = iconState.findIndex(marker=>marker.url === contentParam);
+            if(markerIndex !== -1){
+                const marker = iconState[markerIndex];
+                setCenter(marker.position);
+                handleClick(null, marker, markerIndex);
+            }
+            console.log(contentParam)
         }
-    ];
+    },[handleClick, iconState, searchParams, setContent])
+
+
+    const bounds = [[0, 0], [4767, 3070]];
+    const panBounds = [[-1000, -1000], [5767, 4070]];
+
+
+    useEffect(()=>{
+        console.log(iconState)
+    }, [iconState])
+
+
+
+
+    // const markers = [
+    //     {
+    //         position: [4300,550],
+    //         zIndexOffset: 1000,
+    //         iconGrey: iconMainGrey,
+    //         iconSelect: iconMainSelect,
+    //         name: "Main Entrance",
+    //         url: "main-entrance"
+    //     },
+    //     {
+    //         position: [4035,570],
+    //         zIndexOffset: 1000,
+    //         iconGrey: iconMainGrey,
+    //         iconSelect: iconMainSelect,
+    //         name: "About Ed Broadbent",
+    //         url: "about-ed-broadbent"
+    //     },
+    //     {
+    //         position: [3520,947],
+    //         zIndexOffset: 1000,
+    //         iconGrey: iconAmphitheatreGrey,
+    //         iconSelect: iconAmphitheatreSelect,
+    //         name: "Amphitheatre and Stage",
+    //         url: "amphitheatre-and-stage"
+    //     },
+    //     {
+    //         position: [3200,980],
+    //         zIndexOffset: 1000,
+    //         iconGrey: iconGardenHRGrey,
+    //         iconSelect: iconGardenHRSelect,
+    //         name: "Garden of Human Rights",
+    //         url: "garden-of-human-rights"
+    //     },
+    //     {
+    //         position: [920,1680],
+    //         zIndexOffset: 1000,
+    //         iconGrey: iconOrangeGardenGrey,
+    //         iconSelect: iconOrangeGardenSelect,
+    //         name: "Orange Garden",
+    //         url: "orange-garden"
+    //     },
+    //     {
+    //         position: [465,1570],
+    //         zIndexOffset: 1000,
+    //         iconGrey: iconMainGrey,
+    //         iconSelect: iconMainSelect,
+    //         name: "Parking Entrance",
+    //         url: "parking-entrance"
+    //     }
+    // ];
 
     function UpdateZoom ({center, zoom}) {
         const map = useMap();
@@ -186,12 +310,12 @@ export default function LeafletMap({setContent, popupRef, resetIcons, isIconClic
     
     // maxBounds={panBounds}
     return (
-        <div id="map">
-            <MapContainer crs={L.CRS.Simple} center={center} zoomDelta={0.8} maxBounds={panBounds} zoomSnap={0} zoom={zoom} minZoom={-3} zoomControl={false} closePopupOnClick={false}>
+        <div id="map-wrapper">
+            <MapContainer crs={L.CRS.Simple} center={center} zoomDelta={0.8} maxBounds={panBounds} zoomSnap={0} zoom={zoom} minZoom={-3.1} zoomControl={false} closePopupOnClick={false}>
                 {/* <ZoomTool setZoom={setZoom} /> */}
                 <UpdateZoom center={center} zoom={zoom} />
                 <RecenterAutomatically lat={center[0]} lng={center[1]} />
-                {markers.map((marker, index) =>{
+                {iconState.map((marker, index) =>{
                     return (
                         <Marker 
                             key={index}
@@ -202,7 +326,7 @@ export default function LeafletMap({setContent, popupRef, resetIcons, isIconClic
                                 click: (e)=>{
                                     setCenter(marker.position);
                                     handleClick(e, marker, index);    
-                                    // setZoom(-1);                        
+                                    // setZoom(-1);                     
                                 }
                             }}
                             zIndexOffset={marker.zIndexOffset}>
@@ -218,30 +342,6 @@ export default function LeafletMap({setContent, popupRef, resetIcons, isIconClic
                         </Marker>
                     )
                 })}
-                {/* <MapPin position={[4300,550]} iconURL={"/images/svgs/mapPins/main-grey.svg"} name={"Ed Broadbent Waterfront Park"} />
-                <Marker position={[4300,550]} icon={iconMainGrey} iconSize={[iconSize, iconSize]} zIndexOffset={99999} eventHandlers={{click: (e)=>{console.log("Clicked!")}}}>
-                    <Tooltip className="map-pin" direction="top" key={offset} offset={[0, offset]}  closeButton={false} autoClose={false} closeOnClick={false}>Ed Broadbent Waterfront Park</Tooltip>
-                </Marker>
-                {/* <MapPin position={[4035,570]} iconURL={"/images/svgs/mapPins/main-grey.svg"} name={"About Ed Broadbent"} /> */}
-                {/* <Marker position={[4035,570]} icon={iconMainGrey} zIndexOffset={99999}>
-                    <Tooltip className="map-pin" direction="top" key={offset} offset={[0, offset]}  closeButton={false} autoClose={false} closeOnClick={false}>About Broadbent</Tooltip>
-                </Marker> */}
-                {/* <MapPin position={[3500,954]} iconURL={"/images/svgs/mapPins/amphitheatre-grey.svg"} name={"Amphitheatre and Stage"} /> */}
-                {/* <Marker position={[3500,954]} icon={iconAmphitheatreGrey} zIndexOffset={99999}>
-                    <Tooltip className="map-pin" direction="top" key={offset} offset={[0, offset]} closeButton={false} autoClose={false} closeOnClick={false}>Amphitheatre and Stage</Tooltip>
-                </Marker> */}
-                {/* <MapPin position={[3200,980]} iconURL={"/images/svgs/mapPins/garden-hr-grey.svg"} name={"Garden of Human Rights"} /> */}
-                {/* <Marker position={[3200,980]} icon={iconGardenHRGrey} zIndexOffset={99999}>
-                    <Tooltip className="map-pin" direction="top" key={offset}  closeButton={false} offset={[0, offset]} autoClose={false} closeOnClick={false}>Garden of Human Rights (Simcoe)</Tooltip>
-                </Marker> */}
-                {/* <MapPin position={[920,1680]} iconURL={"/images/svgs/mapPins/orange-garden-grey.svg"} name={"Orange Garden"} /> */}
-                {/* <Marker position={[920,1680]} icon={iconOrangeGardenGrey} zIndexOffset={99999}>
-                    <Tooltip className="map-pin" direction="top" key={offset} closeButton={false} offset={[0, offset]} autoClose={false} closeOnClick={false}>Orange Garden</Tooltip>
-                </Marker> */}
-                {/* <MapPin position={[465,1570]} iconURL={"/images/svgs/mapPins/main-grey.svg"} name={"Parking Entrance"} /> */}
-                {/* <Marker position={[465,1570]} icon={iconMainGrey} zIndexOffset={99999}>
-                    <Tooltip className="map-pin" direction="top" key={offset} closeButton={false} offset={[0, offset]} autoClose={false} closeOnClick={false}>Parking Entrance</Tooltip>
-                </Marker> */}
                 
                 <ImageOverlay url="/Ed-Broadbent-Waterfront-Park/images/svgs/map.svg" bounds={bounds}  />
             </MapContainer>
