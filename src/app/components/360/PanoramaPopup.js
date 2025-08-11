0f04c025393d39569958336cfd8da506d54e4c01
Charlize <145717754+NyxVamour@users.noreply.github.com.>
2025-08-11 09:14:14 -0400

    chore: remove screen resizing because unused

--- src/app/components/360/PanoramaPopup.js
+++ src/app/components/360/PanoramaPopup.js
@@ -7,7 +7,6 @@ import gsap from "gsap";
 import MotionPathPlugin from "gsap/MotionPathPlugin";
 
 export default function PanoramaPopup() {
-    const [screenSize, setScreenSize] = useState({width: 0, height: 0});
     const panoramaPopupRef = useRef(null);
     const iconRef = useRef(null);
     gsap.registerPlugin(MotionPathPlugin);
@@ -16,42 +15,13 @@ export default function PanoramaPopup() {
         panoramaPopupRef.current.style.display = "flex";
     }, [])
 
-    useEffect(()=>{
-        function updateSize(){
-            setScreenSize({
-                width: window.innerWidth,
-                height: window.innerHeight
-            });
-        };
-
-        updateSize();
-        window.addEventListener("resize", updateSize);
-        return(()=> {
-            window.removeEventListener("resize", updateSize);
-        });
-    }, []);
-
-    useEffect(() => {
-        console.log(screenSize);
-    }, [screenSize]);
-
     const handleClick = ()=>{
         panoramaPopupRef.current.style.display = "none";
     };
 
-
-
     return (
         <div className="panorama-popup" ref={panoramaPopupRef} onClick={handleClick}>
             <h1 className="panorama-popup__title">Welcome to the 360 Experience</h1>
-            {/* <Image
-                ref={iconRef}
-                src="/Ed-Broadbent-Waterfront-Park/images/svgs/icons/ar.svg"
-                alt="AR Icon"
-                width={80}
-                height={80}
-                className={`panorama-popup__icon`}
-            /> */}
             <p className="panorama-popup__description">Tap anywhere to start</p>
             <p className="panorama-popup__description"></p>
         </div>
