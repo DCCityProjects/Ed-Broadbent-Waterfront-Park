18faa7858ba353ab7742a5e83623ea8beb5eb968
Charlize <145717754+NyxVamour@users.noreply.github.com.>
2025-08-11 12:14:49 -0400

    feat: add sessionstorage check for preloader
    Made it so you can check sessionstorage so the first time the
    page loads, it holds for a few seconds. Otherwise it acts as a
    pre-loader.

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,43 @@
+"use client"
+
+import { Suspense, useEffect, useState } from "react";
 import LandingPageContent from "./landingPageContent";
+import Loading from "./loading";
 
 
 
-export default async function LandingPage() {
-	const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-	await delay(1000)
+export default function LandingPage() {
+	const [firstLoad, setFirstLoad] = useState(true);
+
+	useEffect(() => {
+		const firstLoadDone = sessionStorage.getItem("firstLoadDone");
+
+		async function showLogoFirstTime(){
+			const delay = (ms) => new Promise((resolve) => {
+				setTimeout(resolve, ms);
+			});
+			await delay(1500);
+			console.log("delay done!");
+			setFirstLoad(false);
+			sessionStorage.setItem("firstLoadDone", "true");
+		};
+
+		if(!firstLoadDone){
+			showLogoFirstTime();
+		} else {
+			setFirstLoad(false);
+		};
+		
+	}, []);
+
+	if(firstLoad){
+		console.log("showing for first load")
+		return <Loading />;
+	}
 
 	return (
-		<LandingPageContent />
+		<Suspense fallback={<Loading />}>
+			<LandingPageContent />
+		</Suspense>
 	);
 }
